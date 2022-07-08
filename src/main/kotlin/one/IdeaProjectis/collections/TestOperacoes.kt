2968208147a494b632439443bba03e99e467fc7f
thiagogurgeira <thiagogurgeira@gmail.com>
2022-07-08 19:55:13 -0300

    creted TestOperacoes

---
+++ src/main/kotlin/one/IdeaProjectis/collections/TestOperacoes.kt
@@ -0,0 +1,27 @@
+package one.IdeaProjectis.collections
+
+fun main() {
+    val salarios = doubleArrayOf(1000.0, 2250.0, 4000.0)
+
+    for (salario in salarios){
+        println(salario)
+    }
+    println("=====================================")
+    println("Maior salario: ${salarios.maxOf { (it) }}")
+    println("Menor salario: ${salarios.minOf { (it) }}")
+    println("Media salario: ${salarios.average()}")
+
+    val salariosMaiorQue2500 = salarios.filter{ it > 2500.0}
+    println("=====================================")
+    salariosMaiorQue2500.forEach{ println(it) }
+
+    println("=====================================")
+    println(salarios.count { it in 2000.0..5000.0 })
+
+    println("=====================================")
+    println(salarios.find { it == 2250.0 })
+    println(salarios.find { it == 500.0 })
+
+    println(salarios.any { it == 1000.0 })
+
+}
